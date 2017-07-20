05864c38c0d40a82a8dd3296bfe8d38f52a66ffd
Karla Fernandes <karla@vitaminak.com.br>
2017-07-20 13:45:02 -0300

    Chaning from Component to Function

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,23 +1,20 @@
 import React, {Component} from "react";
 
-class ListContacts extends Component {
-    render() {
-        let contacts = this.props.contacts;
-        return(
-            <ol className="contact-list">
-                {contacts.map((contact) => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }}/>
-                        <div className="contact-details">
-                            <h1>{contact.name}</h1>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button className="contact-remove">Remove</button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+function ListContacts (props) {
+    return (
+    <ol className="contact-list">
+        {props.contacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+                <div className="contact-avatar" style={{ backgroundImage: `url(${contact.avatarURL})` }}/>
+                <div className="contact-details">
+                    <h1>{contact.name}</h1>
+                    <p>{contact.email}</p>
+                </div>
+                <button className="contact-remove">Remove</button>
+            </li>
+        ))}
+        </ol>
+    )
 }
 
 export default ListContacts;
